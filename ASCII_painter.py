424774e52e50acab766857ca27407b1effb52ebd
V-pakhomov <pakhomov.vasiliy.v@gmail.com>
2020-11-12 20:36:59 +0300

    fix bug with negative in two-colored images

--- ASCII_painter.py
+++ ASCII_painter.py
@@ -1,5 +1,6 @@
 from PIL import Image, ImageDraw
 import sys
+import os
 
 
 ASCII_CHARS = ['#', 'W', '@', '$', '8', '%', 'w', '=', '*', '+', '~', '-', '"', "'", ' ']
@@ -9,23 +10,19 @@ def get_block_color(pix, pix_per_char, i, j):
 	color = 0
 	for x in range(pix_per_char['x']):
 		for y in range(pix_per_char['y']):
-			color += (sum(pix[x + i, y + j][:3])) / 3
+			try:
+				r, g, b = pix[x + i, y + j][:3]
+				pix_color = (r + g + b) / 3
+			except ValueError:
+				pix_color = 255 - pix[x + i, y + j][1]
+			color += pix_color
 	return int(color // (pix_per_char['x'] * pix_per_char['y']))
 
 
-def get_pixels_per_char(width, height):
-	pix_per_char = {}
-	if width > 2*height:
-		pix_per_char['x'] = max((1, int(width // 300)))
-		pix_per_char['y'] = int(pix_per_char['x'] * 2 * height / width)
-	else:
-		pix_per_char['y'] = max((1, int(height // 150)))
-		pix_per_char['x'] = int(pix_per_char['y'] // (2 * width / height))
-	return pix_per_char
-
-
 def image_to_txt(image):
-	ascii_name = 'ascii_' + ''.join(image.split('/')[-1].split('.')[:-1]) + '.txt'
+	_, filename = os.path.split(image)
+	filename = os.path.splitext(filename)[0]
+	ascii_name = 'results/' + filename + '.txt'
 
 	try:
 		image = Image.open(image)
@@ -36,17 +33,16 @@ def image_to_txt(image):
 	width, height = image.size
 	pix = image.load()
 	pix_per_char = {}
-	pix_per_char['x'] = max((1, int(width // 300)))
+	pix_per_char['x'] = max((1, int(width // 200)))
 	pix_per_char['y'] = max((1, pix_per_char['x']*2))
 	with open(ascii_name, 'w') as f:
 		for j in range(0, height, pix_per_char['y']):
 			if j + pix_per_char['y'] > height:
 				break
 			for i in range(0, width, pix_per_char['x']):
-				if i + pix_per_char['x'] <= width:
-					color = get_block_color(pix, pix_per_char, i, j)
-				else:
+				if i + pix_per_char['x'] > width:
 					break
+				color = get_block_color(pix, pix_per_char, i, j)
 				if color == 255:
 					color -= 1
 				char = ASCII_CHARS[int((len(ASCII_CHARS)*color) // 255)]
@@ -59,8 +55,13 @@ def image_to_txt(image):
 
 
 def ascii_painter(*args):
+	if not 'results' in os.listdir():
+		os.mkdir('results')
 	for image in args:
+		_, filename = os.path.split(image)
+		print(f'{filename}:'.upper())
 		image_to_txt(image)
+		print('\n\n')
 
 
 if __name__ == '__main__':
